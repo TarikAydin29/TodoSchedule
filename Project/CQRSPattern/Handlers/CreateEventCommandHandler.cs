ffd55188169c564af93e343d1ac9b80c587b0fed
TarikAydin29 <you@example.com>
2023-08-01 14:40:59 +0300

    Add project files.

---
+++ Project/CQRSPattern/Handlers/CreateEventCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Project.Models;
+
+namespace Project.CQRSPattern.Handlers
+{
+    public class CreateEventCommandHandler
+    {
+        private readonly CalendarContext context;
+
+        public CreateEventCommandHandler(CalendarContext context)
+        {
+            this.context = context;
+        }
+        public void Handle(Event command)
+        {
+            context.Events.Add(new Event
+            {
+               Start=command.Start,
+               End=command.End,
+               Text=command.Text,
+               Color=command.Text
+            });
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Project/CQRSPattern/Handlers/GetEventQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.CQRSPattern.Results;
+using Project.Models;
+
+namespace Project.CQRSPattern.Handlers
+{
+    public class GetEventQueryHandler
+    {
+        private readonly CalendarContext context;
+
+        public GetEventQueryHandler(CalendarContext context)
+        {
+            this.context = context;
+        }
+        public IQueryable<Event> Handle(DateTime start,DateTime end)
+        {
+            var values = context.Events.Where(e => !((e.End <= start) || (e.Start >= end)));
+          
+
+            return values;
+        }
+    }
+}
